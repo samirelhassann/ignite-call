31ead845c848178ef22f052d2141db36d64ffe13
Samir El Hassan <samirelhassann@gmail.com>
2023-04-17 13:45:54 -0300

    Creating the project structure

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { Heading } from "@saturn-design-system/react";
+
+export default function Home() {
+  return (
+    <Heading as="h1">Hello World</Heading>
+  );
+}
